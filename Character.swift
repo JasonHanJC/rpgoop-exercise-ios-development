81b5d278dbc640ddd02cce162650a22edcf6284e
Juncheng Han <namrie1990@gmail.com>
2016-01-25 23:43:35 -0800

    Create the interface

---
+++ Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  rpgoop-exercise-ios-development
+//
+//  Created by Jason miew on 1/25/16.
+//  Copyright © 2016 Jason miew. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+}
