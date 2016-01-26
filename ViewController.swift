f4ebdd5b5ca3e05dbfd26cfb67fd13e565c5ae7a
Juncheng Han <namrie1990@gmail.com>
2016-01-25 23:23:45 -0800

    Init

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rpgoop-exercise-ios-development
+//
+//  Created by Jason miew on 1/25/16.
+//  Copyright © 2016 Jason miew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
